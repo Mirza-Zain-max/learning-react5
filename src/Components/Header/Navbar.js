2b54da614676817fa4dd9492ef806de595a557eb
Zain <mz905543@gmail.com>
2024-11-15 12:09:12 +0500

    4th commit

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -17,19 +17,20 @@ const Navbar1 = () => {
                             <Nav.Link className='ms-4' to="/product">Product</Nav.Link>
                             <Nav.Link className='ms-4' to="/contacts">Contacts</Nav.Link>
                         </Nav>
-                        <Nav className='ms-auto  bg-light'>
-                            <NavDropdown title="Account" className='mt-1' id="basic-nav-dropdown">
+                        <Nav className='ms-auto d-flex justify-content-center align-items-center bg-light'>
+                            <NavDropdown title="Account" className='mt-1 ms-sm-5 ms-md-5 d-flex justify-content-center align-items-center' id="basic-nav-dropdown">
                                 <NavDropdown.Item>
                                     <Link className='ms-4 nav-link text-dark' to="/auth/login">Login</Link>
                                 </NavDropdown.Item>
                                 <NavDropdown.Item>
-                                <Link className='ms-4 nav-link  text-dark' to="/auth/register">Register</Link>
+                                    <Link className='ms-4 nav-link  text-dark' to="/auth/register">Register</Link>
                                 </NavDropdown.Item>
                             </NavDropdown>
                             <Button className='border-0 bg-light mt-2 ms-1'>
                                 <img src={Cart} alt="person" className=' ms-4 my-sm-2 d-flex justify-content-center align-items-center' style={{ height: "20px", width: "20px" }} />
                             </Button>
-                            <span className='ms-3 mt-2 fs-5'>|</span> <Button className='border-0 mt-2 bg-light ms-1'>
+                            <span className='ms-3 mt-2 fs-5 text-center text-md-center'>|</span>
+                            <Button className='border-0 mt-2 bg-light ms-1'>
                                 <img src={Filter} alt="person" className=' ms-4  my-sm-2 d-flex justify-content-center align-items-center' style={{ height: "20px", width: "20px" }} />
                             </Button>
                         </Nav>
