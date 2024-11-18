18d1d5995703120d3d32d4a09b606fb2838b0b92
Zain <mz905543@gmail.com>
2024-11-18 15:51:27 +0500

    7th commit

--- src/Components/Pages/Frontend/Home/Hero.js
+++ src/Components/Pages/Frontend/Home/Hero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import group from '../../Assets/Group.png'
 import { SearchOutlined } from '@ant-design/icons'
+import { Button, Input } from 'antd'
 // import { Fade } from 'react-awesome-reveal'
 
 
@@ -11,7 +12,7 @@ const Hero = () => {
             <Container className='d-flex justify-content-center  align-items-center my-5' style={{ animationDelay: '0ms' }}>
                 <Row className='bg-primary rounded-5'>
                     <Col sm={12} md={12} lg={6}>
-                        <h1 class=" ms-md-3 mt-md-2 display-3 fw-bolder text-center text-lg-start text-black">Buy your <br /> dream plants</h1>
+                        <h1 className=" ms-md-3 mt-md-2 display-3 fw-bolder text-center text-lg-start text-black">Buy your <br /> dream plants</h1>
                         <Col >
                             <h3 className='ms-3 fs-3'>
                                 50+ <span className='' style={{ marginLeft: "150px" }}>100+</span>
@@ -19,11 +20,11 @@ const Hero = () => {
                             </h3>
                         </Col>
                         <Row className="row">
-                            <Col xs={6}  md={12} lg={8} class=" mb-3 w-75 p-sm-3 " style={{ position: "relative" }}>
-                                <input type="text" class="form-control mt-2  ms-sm-3 mx-md-2 py-3 " placeholder="What are you looking for?" />
-                                <button class="btn btn-primary " style={{ position: "absolute", top: "12px", right: "12px", height: "50px" }} type="button">
+                            <Col xs={6}  md={12} lg={8} className=" mb-3 w-75 p-sm-3 " style={{ position: "relative" }}>
+                                <Input type="text" className="form-control mt-2  ms-sm-3 mx-md-2 py-3 " placeholder="What are you looking for?" />
+                                <Button className="btn btn-primary " style={{ position: "absolute", top: "27px", right: "12px", height: "50px" }} type="button">
                                     <SearchOutlined />
-                                </button>
+                                </Button>
                             </Col>
                         </Row>
                     </Col>
